d89f34e053e99a866239d28f6f6e120c839966a5
sebastinalex <45537999+sebastinalex@users.noreply.github.com>
2022-03-21 21:30:24 +0530

    Create SebastinProject.java

---
+++ SebastinProject.java
@@ -0,0 +1,11 @@
+public class SebastinProject {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("Hello zha");
+    System.out.println("Hello Pugazh & Magizh");
+    System.out.println("This code from Git");
+
+ }
+
+}
